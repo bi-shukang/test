c1a4ac6014712737c1477be25fc25930bee3f151
bi-shukang <84639148+bi-shukang@users.noreply.github.com>
2021-06-18 23:15:26 +0800

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,42 @@
+# def test1():
+#     print('游戏开始了')
+#     print('游戏结束了')
+#
+# def test2():
+#     print('test1 开始了')
+#     test1()
+#     print('test1 结束啦')
+#
+# def add1(n,m):
+#     x = 0
+#     for i in range(n,m):
+#         x += i
+#     return x
+# y = add1(1,101)
+# print(y)
+# mul = lambda a, b: b + a
+# print(mul(2, 4))
+#
+#
+students = [
+    {'name':'bishukang','age':21,'score':100,'gender':'male'},
+    {'name':'houquanyu','age':20,'score':80,'gender':'female'},
+    {'name':'yemingzhi','age':20,'score':40,'gender':'female'},
+    {'name':'chengwendong','age':19,'score':90,'gender':'male'}
+]
+# def foo(x):
+#     return x['score']
+#
+# students.sort(key = foo)
+# print(students)
+nums = [5, 32, 25, 6, 19, 8]
+# x = filter(lambda ele:ele>18,nums)
+# print(x)
+# # for a in x:
+# #     print(a,end = ' ')
+#
+# aldut = list(x)
+# print(aldut)
+# m = map(lambda ele:ele+2,nums)
+# m = list(m)
+# print(m)
